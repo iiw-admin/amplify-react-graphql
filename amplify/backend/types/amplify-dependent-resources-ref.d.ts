9ba3d07fb5d8c18bec3c77aa5bd7437ec14bc9df
Aaron Koenes <aaron.koenes@tekton.com>
2022-11-09 20:15:32 -0500

    feat: Support images
    Add image support

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactgraphql1dae77e8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "amplifyreactgraphql": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "imagestorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
