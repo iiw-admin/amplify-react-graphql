95f440e2a7a7e77664de6e2d8b1127e624f8523d
Aaron Koenes <aaron.koenes@tekton.com>
2022-11-12 15:15:45 -0500

    Reset

--- src/App.js
+++ src/App.js
@@ -1,122 +1,25 @@
-import React, {
-  useState,
-  useEffect
-} from "react";
-import "./App.css";
-import "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from 'aws-amplify';
-import {
-  Flex,
-  Heading,
-  View,
-  Badge,
-  Autocomplete,
-  Divider,
-  Link,
-  SearchField,
-  withAuthenticator, Button, TextField,
-} from '@aws-amplify/ui-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-import {
-  createMedia as createMediaMutation,
-} from "./graphql/mutations";
-import { listMedia } from "./graphql/queries";
+import logo from './logo.svg';
+import './App.css';
 
-const App = ({ signOut }) => {
-
-  useEffect(() => {
-    // Do nothing... yet...
-  }, []);
-
-  async function createDBEntry(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const data = {
-      id: Math.floor( Math.random() * 1000000 ),
-      name: form.get("name"),
-    };
-    console.log( "Data", data );
-    console.log( "createMediaMutation", createMediaMutation );
-    await API.graphql({
-      query: createMediaMutation,
-      variables: { input: data },
-    });
-    fetchMedia();
-    event.target.reset();
-  }
-
-
-  async function fetchMedia() {
-    const apiData = await API.graphql({ query: listMedia });
-    const mediaFromAPI = apiData.data.listMedia.items;
-    await Promise.all(
-        mediaFromAPI.map(async (note) => {
-          return note;
-        })
-    );
-    setSearchResults(mediaFromAPI);
-  }
-
-  const [ searchTerm, setSearchTerm ] = useState('');
-  const [ searchResults, setSearchResults ] = useState([
-    { id: "1", label: "Option aaa" },
-    { id: "2", label: "Option aab" },
-    { id: "3", label: "Option aac" },
-    { id: "4", label: "Option aba" },
-    { id: "5", label: "Option abc" },
-    { id: "6", label: "Option bbc" },
-    { id: "7", label: "Option bab" },
-    { id: "8", label: "Option bcc" },
-    { id: "9", label: "Option ccc" },
-    { id: "10", label: "Option ada" },
-    { id: "11", label: "Option eee" },
-    { id: "12", label: "Option abdb" },
-  ]);
-
-  const searchFilter = (element) => {
-    return element.label.indexOf( searchTerm ) > -1;
-  }
-
+function App() {
   return (
-      <View className="App">
-        <Flex className="" direction="row-reverse" wrap="nowrap" alignItems="right">
-          <View>
-            <Badge size={"large"}><FontAwesomeIcon icon={faUser} /></Badge>
-          </View>
-        </Flex>
-        <Flex className="" direction="column" alignItems="center" justifyContent="center">
-          <Heading level={1}>Title</Heading>
-          <SearchField
-              label="Search"
-              placeholder="Search"
-              hasSearchButton={false}
-              hasSearchIcon={true}
-              onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </Flex>
-        <Flex id="searchResults" justifyContent={"center"} direction={"column"}>
-          {searchResults.filter(searchFilter).map((result) => {
-            return (
-                <View key={result.id} className={"searchResult"}>{result.label}</View>
-            )
-          })}
-        </Flex>
-        <View as="form" margin="3rem 0" onSubmit={createDBEntry}>
-          <TextField
-              name="name"
-              placeholder="Entry Name"
-              label="Entry Name"
-              labelHidden
-              variation="quiet"
-              required
-          />
-          <Button type="submit" variation="primary">
-            Add to Database
-          </Button>
-        </View>
-      </View>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
-export default withAuthenticator(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,13 @@
-import { ThemeProvider, Theme } from '@aws-amplify/ui-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Amplify, Storage } from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
-Storage.configure({
-  region: config.aws_user_files_s3_bucket_region,
-  bucket: config.aws_user_files_s3_bucket,
-  identityPoolId: config.aws_user_pools_id,
-  level: "protected",
-});
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const theme: Theme = {
-  // name: 'autocomplete-theme',
-  // tokens: {
-  //   components: {
-  //     autocomplete: {
-  //       menu: {
-  //         option: {
-  //           _active: {
-  //             backgroundColor: {
-  //               value: '#C00',
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
-};
-
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme} colorMode="light">
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
